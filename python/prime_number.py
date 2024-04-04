226c41b11be388a9b1f83f95444f63bfa4bc552d
Krishna Chandra <er.krishnachandra@gmail.com>
2024-04-04 22:21:48 +0530

    Update prime_number.py

--- python/prime_number.py
+++ python/prime_number.py
@@ -1,16 +1,13 @@
-import math
-
-def is_prime(number):
-    if number < 2:
-        return False
-    for i in range(2, int(math.sqrt(number)) + 1):
-        if number % i == 0:
-            return False
-    return True
-
-# Example usage:
-user_input = int(input("Enter a number: "))
-if is_prime(user_input):
-    print(f"{user_input} is a prime number.")
-else:
-    print(f"{user_input} is not a prime number.")
+flag = False
+num = 29
+if num == 1:
+    print(num, "is not a prime number")
+elif num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            flag = True
+            break
+    if flag:
+        print(num, "is not a prime number")
+    else:
+        print(num, "is a prime number")
